82fefa2d59517a8734236da5837ac2ae83355832
ttm <ttm@t.com>
2021-11-08 17:25:52 +0800

    use getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myproject/examples_demo_page.dart';
+import 'package:myproject/first_page.dart';
+import 'package:myproject/my_demo_page.dart';
 import 'package:myproject/router/my_router.dart';
-import 'package:myproject/sliver.dart';
+import 'package:myproject/test_demo_page.dart';
 
-void main() => (runApp(MyApp()));
+//void main() => (runApp(MyApp()));
+
+///use GetX
+void main() => runApp(
+      GetMaterialApp(
+        initialRoute: '/first',
+        getPages: [
+          GetPage(name: '/first', page: () => FirstPage()),
+          GetPage(name: '/my', page: () => MyDemoPage()),
+          GetPage(name: '/examples', page: () => ExamplesDemoPage()),
+          GetPage(name: '/test', page: () => TestDemoPage()),
+        ],
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) {
@@ -13,7 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.blue),
-      home: SliverAppBarDemo(),
+      home: FirstPage(),
     );
   }
 }
